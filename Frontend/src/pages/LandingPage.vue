d17d8e5b9f5947dd5e86cd40c05e4dcc5a0054e0
Mostafa Mohamed <109727053+mostafa569@users.noreply.github.com>
2025-05-07 12:26:00 +0300

    Delete Frontend/src/pages directory

--- Frontend/src/pages/LandingPage.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-
-<template>
-  <div class="container mt-5 text-center">
-    <h1 class="text-primary">Landing Page</h1>
-  </div>
-</template>
-
-<style scoped></style>
--- Frontend/src/pages/NotFound.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-
-<template>
-  <div>
-    <h1 class="text-center text-danger mt-5 fs-1 fw-bold">404 Not Found</h1>
-  </div>
-</template>
-
-<style scoped></style>
