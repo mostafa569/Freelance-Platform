92cb41f8783884461c678dae9532f757713d3c5d
mostafa569 <mostafa.mohamed.aboali76@gmail.com>
2025-05-07 12:10:17 +0300

    admin

--- Backend/app/Http/Controllers/AdminControllers/AdminController.php
+++ Backend/app/Http/Controllers/AdminControllers/AdminController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\AdminControllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EmployerResource;
+use App\Http\Resources\CandidateResource;
 
 class AdminController extends Controller
 {
@@ -46,6 +48,55 @@ class AdminController extends Controller
             return response()->json(['message' => 'Job declined successfully']);
         } else {
             return response()->json(['message' => 'Job not found or already Rejected'], 404);
+        }
+    }
+
+    public function getAllUsers()
+    {
+        try {
+            $users = DB::table('candidates')->paginate(15);
+            if (!$users) {
+                return response()->json(['message' => 'No users found'], 404);
+            }
+            return CandidateResource::collection($users);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'No users found'], 404);
+        }
+    }
+        public function getAllEmployers()
+        {
+            try {
+                $employers = DB::table('employers')->paginate(15);
+                if (!$employers) {
+                    return response()->json(['message' => 'No employers found'], 404);
+                }
+                return EmployerResource::collection($employers);
+            } catch (\Exception $e) {
+                return response()->json(['message' => 'No employers found'], 404);
+            }
+        }
+    public function deleteUser($id)
+    {
+        try {
+            $user = DB::table('candidates')->where('id', $id)->delete();
+            if (!$user) {
+                return response()->json(['message' => 'User not found'], 404);
+            }
+            return response()->json(['message' => 'User deleted successfully'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+    }
+    public function deleteEmployer($id)
+    {
+        try {
+            $employer = DB::table('employers')->where('id', $id)->delete();
+            if (!$employer) {
+                return response()->json(['message' => 'Employer not found'], 404);
+            }
+            return response()->json(['message' => 'Employer deleted successfully'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Employer not found'], 404);
         }
     }
     
--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -14,5 +14,9 @@ Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
     Route::get('allJobs', [AdminController::class, 'getAllJobs']);
     Route::post('approveJob/{id}',[AdminController::class,'approveJob']);
     Route::post('declineJob/{id}',[AdminController::class,'declineJob']);
+    Route::get('get-all-users', [AdminController::class, 'getAllUsers']);
+Route::get('get-all-employers', [AdminController::class, 'getAllEmployers']);
+Route::delete('delete-user/{id}', [AdminController::class, 'deleteUser'])->where('id', '[0-9]+');
+Route::delete('delete-employer/{id}', [AdminController::class, 'deleteEmployer'])->where('id', '[0-9]+');
 
 });
